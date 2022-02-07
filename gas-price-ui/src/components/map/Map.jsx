7934d39f76bee482270eefce389def379ec7196a
leoBurette <leo.burette@etu.unice.fr>
2022-02-07 15:34:38 +0100

    Add map

--- gas-price-ui/src/components/map/Map.jsx
+++ gas-price-ui/src/components/map/Map.jsx
@@ -1,19 +1,31 @@
 import React from "react";
-import { Typography,Button } from '@mui/material';
-import { useNavigate } from "react-router";
-
+// import { useNavigate } from "react-router";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+// import 'leaflet/dist/leaflet.css';
+import './Map.css';
 export default function Map() {
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
+
+    const mapStyle = {
+        height: '100%',
+    }
 
     return (
-        <>
-            <Typography>Bite</Typography>
-            <Button onClick={() => {
-                navigate({ pathname: '/statistics'})
-            } } >
-                Click
-            </Button>
+        <>  
+            <div id="map" style={mapStyle}>
+                <MapContainer center={[51.505, -0.09]} zoom={13}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
         </>
     )
 }
