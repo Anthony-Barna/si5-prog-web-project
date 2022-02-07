540fdf2bd6d5a2c1b5a615c0cd0a3a76c716c3c4
leoBurette <leo.burette@etu.unice.fr>
2022-02-07 16:27:36 +0100

    Passage en composant

--- gas-price-ui/src/components/map/Map.jsx
+++ gas-price-ui/src/components/map/Map.jsx
@@ -3,29 +3,36 @@ import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 // import 'leaflet/dist/leaflet.css';
 import './Map.css';
-export default function Map() {
 
-    // const navigate = useNavigate();
-
-    const mapStyle = {
-        height: '100%',
+export default class Map extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            position: (props.position) ? props.position : [43.27, 5.40]
+        }
     }
 
-    return (
-        <>  
-            <div id="map" style={mapStyle}>
-                <MapContainer center={[51.505, -0.09]} zoom={13}>
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Marker position={[51.505, -0.09]}>
-                        <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                        </Popup>
-                    </Marker>
-                </MapContainer>
-            </div>
-        </>
-    )
+    render(){
+        const mapStyle = {
+            height: '100%',
+        }
+        
+        return (
+            <>  
+                <div id="map" style={mapStyle}>
+                    <MapContainer center={this.state.position} zoom={9}>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={this.state.position}>
+                            <Popup>
+                                Your position
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
+            </>
+        )
+    }
 }
