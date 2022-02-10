f4cc93fff99366e4848f153bc793d79840afcb95
Hadrien Bonato-Pape <hadrien.bonato--pape@etu.unice.fr>
2022-02-10 09:45:15 +0100

    Using good practices for fetching data

--- gas-price-ui/src/components/map/Map.jsx
+++ gas-price-ui/src/components/map/Map.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import { Typography,Button } from '@mui/material';
 import { useNavigate } from "react-router";
+import { useState } from "react";
+
 
 export default function Map() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [gasStation, setResults] = useState([]);
 
     const navigate = useNavigate();
+
+    const getListOfGasStation = ((limit) => {
+        fetch(`http://localhost:8080/api/sales-points?limit=${limit}`)
+            .then((response) => response.json())
+            .then((jsonResponse) => {
+                console.log(jsonResponse);
+                setIsLoaded(true);
+                setResults(jsonResponse);
+            }, (error) => {
+                setIsLoaded(true);
+                setError(error);
+            })
+    })
+
     const listOfGasStation = getListOfGasStation(5);
 
     return (
@@ -17,13 +36,4 @@ export default function Map() {
             </Button>
         </>
     )
-}
-
-function getListOfGasStation(limit) {
-    fetch(`http://localhost:8080/api/sales-points?limit=${limit}`)
-        .then((response) => response.json())
-        .then((jsonResponse) => {
-            console.log(jsonResponse);
-            return jsonResponse;
-        })
 }
