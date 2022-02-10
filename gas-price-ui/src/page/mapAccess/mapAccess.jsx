54d4b3501f51fc877c5e66fc7ab2c62c159c0d19
leoBurette <leo.burette@etu.unice.fr>
2022-02-10 11:55:41 +0100

    #15 : add some color

--- gas-price-ui/src/page/mapAccess/mapAccess.jsx
+++ gas-price-ui/src/page/mapAccess/mapAccess.jsx
@@ -1,10 +1,12 @@
 import Map from "../../components/map/Map"
 import React from "react";
 import "./mapAccess.css";
-import { MarkerModel } from "../../components/map/markerModel";
+import { MarkerModel } from "../../components/model/markerModel";
+import { colors } from "../../components/model/colorsGasStation";
+
 
 export function MapAccess () {
-    let markersList = [new MarkerModel(43.27, 5.40, "Your position"), new MarkerModel(45.27, 7.40, "Your position2")]
+    let markersList = [new MarkerModel(43.27, 5.40, "Your position"), new MarkerModel(45.27, 7.40, "Your position2", colors.GREEN), new MarkerModel(44.27, 7.40, "WOW", colors.RED)]
     // let markersList = []
 
     return (
