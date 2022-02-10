587fc0216750ae7fc1b9f268cbee976e54c5e310
Hadrien Bonato-Pape <hadrien.bonato--pape@etu.unice.fr>
2022-02-10 10:30:45 +0100

    Finish function to request the api with variable arguments #8

--- gas-price-ui/src/components/map/Map.jsx
+++ gas-price-ui/src/components/map/Map.jsx
@@ -11,8 +11,8 @@ export default function Map() {
 
     const navigate = useNavigate();
 
-    const getListOfGasStation = ((limit) => {
-        fetch(`http://localhost:8080/api/sales-points?limit=${limit}`)
+    const getAllGasStation = (() => {
+        fetch(`http://localhost:8080/api/sales-points`)
             .then((response) => response.json())
             .then((jsonResponse) => {
                 console.log(jsonResponse);
@@ -24,7 +24,33 @@ export default function Map() {
             })
     })
 
-    const listOfGasStation = getListOfGasStation(5);
+    // limit:int, road:string, distance:number, price:number, fuel:string
+    const getListOfGasStation = (( limit = 5, road= null, distance= null, price= null, fuel= null) => {
+        let request = '?limit=${limit}';
+        if(road != null) { request = request + '&road=' + road }
+        if(distance != null) { request = request + '&distance=' + distance }
+        if(price != null) { request = request + '&price=' + price }
+        if(fuel != null) { request = request + '&fuel=' + fuel }
+
+        fetch(`http://localhost:8080/api/sales-points${request}`)
+            .then((response) => response.json())
+            .then((jsonResponse) => {
+                console.log(jsonResponse);
+                setIsLoaded(true);
+                setResults(jsonResponse);
+            }, (error) => {
+                setIsLoaded(true);
+                setError(error);
+            })
+    })
+
+    // JE SAIS PAS ENCORE FAIRE DE TESTS AVEC REACT ALORS LAISSEZ MOI ESSAYER
+    const listOfGasStationNoArg = getListOfGasStation();
+    const listOfGasStationLim = getListOfGasStation(5);
+    const listOfGasStationRoad = getListOfGasStation(5, "Toulon");
+    const listOfGasStationDistance = getListOfGasStation(5, "Toulon", 3);
+    const listOfGasStationPrice = getListOfGasStation(5, null, null, 1.5);
+    const listOfGasStationFuel = getListOfGasStation(5, null, null, null, "SP98");
 
     return (
         <>
