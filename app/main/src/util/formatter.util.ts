4d07ff0953824de00dd1660d4992fa14ffaf34e2
Anthony Barna <anthony.barna@etu.univ-cotedazur.fr>
2022-02-21 14:46:28 +0100

    correcting positions

--- app/main/src/util/formatter.util.ts
+++ app/main/src/util/formatter.util.ts
@@ -1,46 +1,15 @@
 export class FormatterUtil {
+
   private static readonly FRANCE_MIN_LONGITUDE = -10;
   private static readonly FRANCE_MAX_LONGITUDE = 10;
   private static readonly FRANCE_MIN_LATITUDE = 40;
   private static readonly FRANCE_MAX_LATITUDE = 55;
 
   public static formatLongitude(longitude: number): number {
-    while (longitude < this.FRANCE_MIN_LONGITUDE) {
-      if (longitude < 0) {
-        longitude /= 10;
-      }
-      if (longitude > 0) {
-        longitude *= 10;
-      }
-    }
-    while (longitude > this.FRANCE_MAX_LONGITUDE) {
-      if (longitude > 0) {
-        longitude /= 10;
-      }
-      if (longitude < 0) {
-        longitude *= 10;
-      }
-    }
-    return longitude;
+    return longitude/100000;
   }
 
   public static formatLatitude(latitude: number): number {
-    while (latitude < this.FRANCE_MIN_LATITUDE) {
-      if (latitude < 0) {
-        latitude /= 10;
-      }
-      if (latitude > 0) {
-        latitude *= 10;
-      }
-    }
-    while (latitude > this.FRANCE_MAX_LATITUDE) {
-      if (latitude > 0) {
-        latitude /= 10;
-      }
-      if (latitude < 0) {
-        latitude *= 10;
-      }
-    }
-    return latitude;
+    return latitude/100000;
   }
 }
