3f8a36b133622787ea8f6fe41d474740d1dee86a
Anthony Barna <anthony.barna@etu.univ-cotedazur.fr>
2022-02-27 15:39:13 +0100

    adding reports

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Team members
 - [Anthony Barna](https://github.com/Anthony-Barna)
 - [Leo Burette](https://github.com/LeoBurette)
-- [Hadrien Bonato Pape](https://github.com/Hadrien-Bonato-Pape)
+- [Hadrien Bonato--Pape](https://github.com/Hadrien-Bonato-Pape)
 - [Jérôme Froment](https://github.com/JeromeFroment)
 
 ## Frontend repository
@@ -18,7 +18,7 @@ Run the project
 ```bash
 $ ./run.sh
 ```
-
+The web app is now available at : [http://localhost:8080](http://localhost:8080)
 ## You can see the api documentation on :
 ```text
 GET localhost:8080/api
---
+++ app/client-build/precache-manifest.2ab0a9070d9f746d016c25068b22eac8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30980e7b08f0ac9ba47db45eb9d0351",
+    "url": "/index.html"
+  },
+  {
+    "revision": "252460695e0205febdce",
+    "url": "/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "6da8c36aba84a9a4bde7",
+    "url": "/static/css/main.8974eb0e.chunk.css"
+  },
+  {
+    "revision": "252460695e0205febdce",
+    "url": "/static/js/2.4a512f74.chunk.js"
+  },
+  {
+    "revision": "18d03fd554473a9d8a94bd3b4f809256",
+    "url": "/static/js/2.4a512f74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b7054a68fc5f33489cf",
+    "url": "/static/js/3.4fcf7bdd.chunk.js"
+  },
+  {
+    "revision": "6da8c36aba84a9a4bde7",
+    "url": "/static/js/main.cef51b89.chunk.js"
+  },
+  {
+    "revision": "09b4c79e5d4ed0c69fc5",
+    "url": "/static/js/runtime-main.4bf97f2b.js"
+  },
+  {
+    "revision": "b4e47a4d59faa79593f6d335154c92f6",
+    "url": "/static/media/gas-station-green.b4e47a4d.png"
+  },
+  {
+    "revision": "b512517ee47bf661d6946ee165db1fbe",
+    "url": "/static/media/gas-station-red.b512517e.png"
+  },
+  {
+    "revision": "920f0bdfcb47edcb2ddc8ecc7f3de155",
+    "url": "/static/media/gas-station.920f0bdf.png"
+  },
+  {
+    "revision": "c0fbf34595e98274e202702bce00030d",
+    "url": "/static/media/pinContrast.c0fbf345.png"
+  }
+]);
